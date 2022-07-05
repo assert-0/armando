8f00bfb49e972cc3dd450ae2ad7708b0f52e0e4d
Lucija Aleksic <lucijaaleksic16@gmail.com>
2022-07-05 15:11:04 +0200

    add rule for re-enganing buyers

--- services/rule_engine/src/main/java/agents/Homesmart.java
+++ services/rule_engine/src/main/java/agents/Homesmart.java
@@ -2,17 +2,21 @@ package agents;
 
 import com.mindsmiths.ruleEngine.model.Agent;
 import com.mindsmiths.telegramAdapter.TelegramAdapterAPI;
+import java.util.Date;
 import lombok.*;
 
 @Getter
 @Setter
 public class Homesmart extends Agent {
+    private Date lastInteractionTime;
 
     public Homesmart() {
+        lastInteractionTime = new Date();
     }
 
     public Homesmart(String connectionName, String connectionId) {
         super(connectionName, connectionId);
+        lastInteractionTime = new Date();
     }
 
 
