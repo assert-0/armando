2ff033fce0da6669315f42b9be5860de4435c8fc
SolidStarman <leon.zrnic2000@gmail.com>
2022-07-07 12:05:31 +0200

    added protoagent and agentagent

---
+++ services/rule_engine/src/main/java/agents/AgentAgent.java
@@ -0,0 +1,19 @@
+package agents;
+
+import java.util.Date;
+import java.util.Arrays;
+
+import lombok.*;
+
+import com.mindsmiths.ruleEngine.model.Agent;
+import com.mindsmiths.telegramAdapter.TelegramAdapterAPI;
+import com.mindsmiths.telegramAdapter.KeyboardData;
+import com.mindsmiths.telegramAdapter.KeyboardOption;
+
+
+@Getter
+@Setter
+
+public class AgentAgent {
+    
+}
---
+++ services/rule_engine/src/main/java/agents/ProtoAgent.java
@@ -0,0 +1,37 @@
+package agents;
+
+import java.util.Date;
+import java.util.Arrays;
+
+import lombok.*;
+
+import com.mindsmiths.ruleEngine.model.Agent;
+import com.mindsmiths.telegramAdapter.TelegramAdapterAPI;
+import com.mindsmiths.telegramAdapter.KeyboardData;
+import com.mindsmiths.telegramAdapter.KeyboardOption;
+
+
+@Getter
+@Setter
+public class ProtoAgent extends Agent{
+
+    public void sendMessage(String text) {
+        String chatId = getConnections().get("telegram");
+        TelegramAdapterAPI.sendMessage(chatId, text);
+    }
+
+    public void sendRoleAssignment() {
+        TelegramAdapterAPI.sendMessage(
+            connections.get("telegram"),
+            "What is your role?\n",
+            new KeyboardData(
+                "5982093762831",
+                Arrays.asList(
+                    new KeyboardOption("USER", "User"),
+                    new KeyboardOption("AGENT", "Agent"),
+                    new KeyboardOption("HITL", "Hitl")
+                )
+            )
+        );
+    }
+}
